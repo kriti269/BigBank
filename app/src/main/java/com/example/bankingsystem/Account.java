a67b2466532df02d7f7cbcc0093e0098a31d7ea9
Kriti <kriti269@gmail.com>
2021-08-12 13:40:38 -0400

    transfer activity changes. transfer to other user changes.

--- app/src/main/java/com/example/bankingsystem/Account.java
+++ app/src/main/java/com/example/bankingsystem/Account.java
@@ -96,4 +96,50 @@ class AccountOperations
         }
         return "";
     }
+
+    public static boolean checkUser(String name){
+        for(Account account: MainActivity.accountsList){
+            if(!MainActivity.loggedInUser.getName().equalsIgnoreCase(name)
+                    && name.equalsIgnoreCase(account.getUser().getName()))
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean checkAccount(String accountNumber){
+        for(Account account: MainActivity.accountsList){
+            if(!MainActivity.loggedInUser.getName().equalsIgnoreCase(account.getUser().getName())
+                    && accountNumber.equalsIgnoreCase(account.getAccountId()))
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean checkUserAccount(String user, String accountId){
+        for(Account account: MainActivity.accountsList){
+            if(!MainActivity.loggedInUser.getName().equalsIgnoreCase(account.getUser().getName())
+                    && accountId.equalsIgnoreCase(account.getAccountId()) && user.equalsIgnoreCase(account.getUser().getName()))
+                return true;
+        }
+        return false;
+    }
+
+    public static String depositUserAmount(String amountString, String accountId) {
+        double amount = Double.parseDouble(amountString);
+        Account account = getUserAccount(accountId);
+        if(account == null) {
+            return "Please select valid account to deposit";
+        } else {
+            account.setBalance(account.getBalance() + amount);
+        }
+        return "";
+    }
+
+    public static Account getUserAccount(String accountId) {
+        for(Account account: MainActivity.accountsList){
+            if(accountId.equalsIgnoreCase(account.getAccountId()))
+                return account;
+        }
+        return null;
+    }
 }
